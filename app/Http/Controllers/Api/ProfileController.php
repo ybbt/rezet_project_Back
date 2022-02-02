e88bf27f809e1f3dd7e4825d93a162968768a0da
ybbt <olexiy.k@i.ua>
2022-02-02 18:00:19 +0200

    Потрібна валідація

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -48,26 +48,34 @@ class ProfileController extends Controller
 
     public function newAvatar(Request $request)
     {
-        // dd($request);
-        $path = $request->avatar->store('avatars');
-        // return Storage::disk('local')->get($path);
-        Storage::delete(auth()->user()->profile->avatar_path);
-        $avatar_path = ["avatar_path" => $path];
-        auth()->user()->profile()->update($avatar_path);
-        return $path;
+        // dump($request->avatar);
+        if ($request->avatar === null) {
+            Storage::delete(auth()->user()->profile->avatar_path);
+            $avatar_path = ["avatar_path" => null];
+            auth()->user()->profile()->update($avatar_path);
+            response()->noContent();
+        } else {
+            // dd($request->avatar);
+            $path = $request->avatar->store('avatars');
+            // return Storage::disk('local')->get($path);
+            Storage::delete(auth()->user()->profile->avatar_path);
+            $avatar_path = ["avatar_path" => $path];
+            auth()->user()->profile()->update($avatar_path);
+            return $path;
+        }
     }
 
-    // --- тимчасово для тесту
-    public function getAvatar()
-    {
-        // dd(auth()->user()->profile->avatar_path);
-        // return Storage::disk('local')->get('images/GNSLdsM61xCf7dqvXmwOpYqu3xgiyXoAxTc9VbQU.png');
-        // $url = Storage::url("ii6M2s2obYHY6P8nWD6oFQTfKq17sboVLBC54wod.png");
-        // dd($url);
-        // return response()->file('/images/oe4icjnf0NawQjixlYjvbwPPKONs2u0RwsQkpmgI.png');
-        $url = asset(auth()->user()->profile->avatar_path);
-        return response($url);
-    }
+    // // --- тимчасово для тесту
+    // public function getAvatar()
+    // {
+    //     // dd(auth()->user()->profile->avatar_path);
+    //     // return Storage::disk('local')->get('images/GNSLdsM61xCf7dqvXmwOpYqu3xgiyXoAxTc9VbQU.png');
+    //     // $url = Storage::url("ii6M2s2obYHY6P8nWD6oFQTfKq17sboVLBC54wod.png");
+    //     // dd($url);
+    //     // return response()->file('/images/oe4icjnf0NawQjixlYjvbwPPKONs2u0RwsQkpmgI.png');
+    //     $url = asset(auth()->user()->profile->avatar_path);
+    //     return response($url);
+    // }
 
     public function deleteAvatar()
     {
